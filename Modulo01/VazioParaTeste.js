c5a769d75abe4ce1119c59cdafd7523746af4ea8
seuUserNamebrunocouto <bruno.couto.joao@gmail.com@gmail.com>
2023-08-20 21:31:00 -0300

    Novas alterações

---
+++ Modulo01/VazioParaTeste.js
@@ -0,0 +1,91 @@
+// Praticando exercicios 
+// let P = 79;
+// let A = 1.79;
+// let imc = P / (A*A);
+// console.log(imc.toFixed(2));
+
+// let temPo = 10 ;
+// let distanCia = 500 ;
+// let velociDade = distanCia / temPo;
+
+// console.log(`a velocidade percorrida e ${velociDade}`)
+
+
+// let raioBase = 3;
+// let alturaDoCil = 1;
+// let areaTotal = 2*Math.PI*raioBase+(raioBase+alturaDoCil);
+// console.log(areaTotal)
+
+// const jogada1 = "pedra";
+// const jogada2 = "tesoura";
+
+// if (jogada1 === "pedra" && jogada2 === "tesoura"){
+//     console.log("Pedra Venceu ");
+// } else if (jogada1 === "papel" && jogada2 === "pedra") {
+//     console.log("Papel vence pedra");
+//   } else if (jogada1 === "tesoura" && jogada2 === "papel") {
+//     console.log("Tesoura vence papel");
+//   } else {
+//     console.log("Empate");
+//   }
+
+// const alturaEmCem = 185;
+
+// if (alturaEmCem === 185){
+//     console.log("Aprovado");
+//     }else{ 
+//         console.log("Reprovado");
+// }
+
+// const nota = 8.5;
+
+// if(nota >= 9 && nota <= 10){
+//     console.log("Voce tirou nota A");
+// }else if (nota >=8 && nota <= 8.9){
+//     console.log("Voce tirou nota B");
+// }else if (nota >=6 && nota <= 7.9){
+//     console.log("Voce tirou nota C");
+// }else if (nota >=4 && nota <=5.9 ){
+//     console.log("Voce tirou D")
+// }else if (nota <= 4){
+//     console.log("Voce tirou E");
+// }
+
+//const valorDoProduto = 13000;
+// let credito	 = 0.05;
+// let cheque	= 0.03;
+// let debito_ou_dinheiro =0.10;
+// console.log 
+
+// //tipo de pagamento (dinheiro, credito, debito, cheque).
+// const tipoDePagamento = "credito";
+
+// //valor da mercadoria (centavos)
+// const valorDoProduto = 13000;
+
+// valorFinal = valorDoProduto - (valorDoProduto*credito);
+// console.log(valorFinal.toFixer(2))
+
+// const tipoDePagamento = "credito";
+// const valorDoProduto = 13000;
+
+// let valorFinal = 0;
+
+// if (tipoDePagamento === "credito") {
+// valorFinal = valorDoProduto - (valorDoProduto * 0.05);
+// } else if (tipoDePagamento === "cheque") {
+// valorFinal = valorDoProduto - (valorDoProduto * 0.03);
+// } else if (tipoDePagamento === "debito" || tipoDePagamento === "dinheiro") {
+// valorFinal = valorDoProduto - (valorDoProduto * 0.10);
+// }
+
+// console.log(valorFinal.toFixed(2));
+
+//valor do produto comprado.
+const valorDoProduto = 100000;
+
+//quantidade de parcelas
+const quantidadeDoParcelamento = 10;
+
+//valor pago
+const valorPago = 300;
